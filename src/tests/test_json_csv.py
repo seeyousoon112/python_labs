af546e3667c8fd696c617fa99238bfa6f2d1c949
seeyousoon112 <ksaveralexfold28@mail.ru>
2025-11-28 11:24:24 +0300

    lzs

--- src/tests/test_json_csv.py
+++ src/tests/test_json_csv.py
@@ -57,7 +57,7 @@ class TestJsonToCsv:
 
         json_path.write_text("", encoding="utf-8")
 
-        with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match='Файл пустой'):
             json_to_csv(str(json_path), str(csv_path))
 
     def test_json_to_csv_invalid_json(self, tmp_path):
